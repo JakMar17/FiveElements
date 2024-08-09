a9e1c88ee1a2a9b05c91654d76819f781d9e782c
JakMar17 <jakob.marusic17@gmail.com>
2024-08-09 11:17:39 +0200

    vanillaJS

--- 02-bot/index.js
+++ 02-bot/index.js
@@ -114,13 +114,22 @@ bot.command('flipGame', ctx => {
 bot.command("inlinekb", ctx =>
 	ctx.reply(
 		"Launch mini app from inline keyboard!",
-		Markup.inlineKeyboard([Markup.button.webApp("Launch", "https://www.google.com")]),
+		Markup.inlineKeyboard([Markup.button.webApp("Launch", "http://t.me/FiveElementsJakMarsBot/FiveElementsJakMarsMiniApp")]),
 	),
 );
 
 bot.on("inline_query", ctx =>
 	ctx.answerInlineQuery([], {
-		button: { text: "Launch", web_app: { url: WEB_APP_URL } },
+		button: { text: "Launch", web_app: { url: "http://t.me/FiveElementsJakMarsBot/FiveElementsJakMarsMiniApp" } },
+	}),
+);
+
+bot.command("setmenu", ctx =>
+	// sets Web App as the menu button for current chat
+	ctx.setChatMenuButton({
+		text: "Launch",
+		type: "web_app",
+		web_app: { url: 'https://fiveelementsjakmarsminiapp.netlify.app/' },
 	}),
 );
 
